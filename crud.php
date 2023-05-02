a271358a1ab9df82269d9d8be8565a5cd6bd8a66
GAbizuto <gabrielbrach9555@gmail.com>
2023-05-02 10:54:17 -0300

    ,;k

---
+++ crud.php
@@ -0,0 +1,58 @@
+<?php
+    include("conecta.php");
+    // PARA PEGAR O TEXTO DOS INPUTS
+    $matricula = $_POST["matricula"];
+    $nome  = $_POST["nome"];
+    $idade = $_POST["idade"];
+
+    //se cliclou no botão INSERIR:
+
+    if(isset($_POST["INSERIR"]))
+    {
+    $comando = $pdo->prepare("INSERT INTO alunos VALUES ('$matricula','$nome',$idade)");
+    
+    $resultado = $comando->execute();
+
+    //para voltar no formulário:
+    
+    header("Location: cadastro.html");
+    }
+    
+    if(isset($_POST["EXCLUIR"]))
+    {
+        $comando = $pdo->prepare("DELETE FROM alunos WHERE matricula = $matricula");
+       
+       $resultado = $comando->execute();
+   
+       //para voltar no formulário:
+       
+       header("Location: cadastro.html");
+    }
+    if(isset($_POST["ALTERAR"]))
+    {
+        $comando = $pdo->prepare("UPDATE alunos SET nome='$nome',idade=$idade WHERE matricula=$matricula ");
+       
+       $resultado = $comando->execute();
+   
+       //para voltar no formulário:
+       
+       header("Location: cadastro.html");
+    }
+    if(isset($_POST["LISTAR"]))
+    {
+        $comando = $pdo->prepare("SELECT * FROM alunos ");
+       
+       $resultado = $comando->execute();
+       while( $linha = $comando -> fetch()){
+        $m = $linhas ["matricula"];
+        $n = $linhas ["nome"];
+        $i = $linhas ["idade"];
+        echo("Matricula; $m Nome: $n Idade: $i<br>")
+       }
+   
+       //para voltar no formulário:
+       
+     
+    }
+  
+?>
